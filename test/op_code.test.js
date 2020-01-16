fcd9a660c15d999d2cb0dfc4eba27ff6d188bc2a
libitx <libitx@protonmail.com>
2020-01-16 21:08:45 +0000

    Prebuild and export all op codes for more convinient access.

--- test/op_code.test.js
+++ test/op_code.test.js
@@ -1,35 +1,53 @@
 const { assert } = require('chai')
-const { OpCode } = require('../index')
+const { OpCode, Ops } = require('../index')
 
 
-describe('OpCode()', () => {
+describe('new OpCode()', () => {
   it('must instantiate an OpCode by string', () => {
-    const opcode = OpCode('OP_RETURN')
-    assert.equal(opcode.op, 'OP_RETURN')
-    assert.equal(opcode.code, 106)
-    assert.isTrue(opcode.isValid)
-  })
-
-  it('must instantiate an OpCode by number', () => {
-    const opcode = OpCode(106)
+    const opcode = new OpCode('OP_RETURN')
     assert.equal(opcode.op, 'OP_RETURN')
     assert.equal(opcode.code, 106)
     assert.isTrue(opcode.isValid)
   })
 
   it('must throw an error with invalid string', () => {
-    assert.throws(_ => OpCode('fail'), 'Invalid OpCode')
-  })
-
-  it('must throw an error with invalid number', () => {
-    assert.throws(_ => OpCode(20), 'Invalid OpCode')
+    assert.throws(_ => new OpCode('fail'), 'Invalid OpCode')
   })
 })
 
 describe('OpCode#toBuffer()', () => {
   it('must return a data buffer', () => {
-    const buf = OpCode('OP_RETURN').toBuffer()
+    const buf = new OpCode('OP_RETURN').toBuffer()
     assert.instanceOf(buf, Buffer)
     assert.lengthOf(buf, 1)
+  })
+})
+
+
+describe('Ops', () => {
+  it('must be a frozen object', () => {
+    assert.typeOf(Ops, 'object')
+    Ops.OP_RETURN = null
+    Ops.OP_FAKE_IT = 'testing'
+    assert.instanceOf(Ops.OP_RETURN, OpCode)
+    assert.isUndefined(Ops.OP_FAKE_IT)
+  })
+})
+
+
+describe('Ops.byCode()', () => {
+  it('must find OpCode by number', () => {
+    const opcode = Ops.byCode(106)
+    assert.equal(opcode.op, 'OP_RETURN')
+    assert.equal(opcode.code, 106)
+    assert.isTrue(opcode.isValid)
+  })
+
+  it('must return null with invalid number', () => {
+    assert.isNull(Ops.byCode(20))
+  })
+
+  it('must throw error with invalid number and err option', () => {
+    assert.throws(_ => Ops.byCode(20, true), 'Invalid OpCode')
   })
 })
